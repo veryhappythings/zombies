24ca14aeea2f625d4fcd5af22746e0db8f2923ad
mac <mac@veryhappythings.co.uk>
2010-12-30 17:04:57 +0000

    Adds another menu scenario

--- features/menu.feature
+++ features/menu.feature
@@ -7,10 +7,17 @@ Feature: Menu
                 Given that I have a menu up
                 And I have selected "new game"
                 When I press return
-                Then the game should start
+                Then I should be playing the game
 
         Scenario: Quit the game
                 Given that I have a menu up
                 And I have selected "quit"
                 When I press return
                 Then the window should close
+
+        Scenario: Resume the game
+                Given that I am playing a game
+                And I have brought up the menu
+                And I have selected "resume"
+                When I press return
+                Then I should be playing the game
--- features/step_definitions/zombie_steps.rb
+++ features/step_definitions/zombie_steps.rb
@@ -8,6 +8,7 @@ end
 After do
 end
 
+# Givens
 Given /^that I have a menu up$/ do
   @window = GameWindow.new
 end
@@ -16,15 +17,25 @@ Given /^I have selected "([^"]*)"$/ do |item|
   @window.current_game_state.select_item item
 end
 
+Given /^that I am playing a game$/ do
+  @window.new_game!
+end
+
+Given /^I have brought up the menu$/ do
+  @window.button_down(Gosu::Button::KbEscape)
+end
+
+# Whens
 When /^I press return$/ do
     @window.current_game_state.button_down(Gosu::Button::KbReturn)
 end
 
-Then /^the game should start$/ do
-  @window.current_game_state.class.should == PlayingState
-end
-
+# Thens
 Then /^the window should close$/ do
   # I don't currently have a way to find out if the window closed or not.
+end
+
+Then /^I should be playing the game$/ do
+  @window.current_game_state.class.should == PlayingState
 end
 
